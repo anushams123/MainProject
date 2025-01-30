1a3b0b400935f7558e3ccb6dfe9f93ec0d371918
anushams123 <anushasadamani@gmail.com>
2025-01-30 17:46:24 +0530

    Add files via upload

---
+++ StepInAppApplication.java
@@ -0,0 +1,13 @@
+package com.stepIn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepInAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepInAppApplication.class, args);
+	}
+
+}
